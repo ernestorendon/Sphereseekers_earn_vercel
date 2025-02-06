28fc515efcf4e156f24efc5c742ca07d45142f3b
Ernesto <ernestorendon@ufl.edu>
2025-02-06 12:35:47 -0500

    Fix ability of ball to 'fly' when camera is looking at marble from above

--- sphereseekers/Scripts/Movement/ball.gd
+++ sphereseekers/Scripts/Movement/ball.gd
@@ -6,23 +6,42 @@ extends RigidBody3D
 
 
 func _physics_process(delta):
+	# Set speed limit for the ball's velocity
 	linear_velocity.x = clamp(linear_velocity.x, -max_velocity, max_velocity)
 	linear_velocity.z = clamp(linear_velocity.z, -max_velocity, max_velocity)
 	
+	# Process physics of movement
 	movement(delta)
 	pass
 
+"""
+TODO:
+	As of now, the movement vector is calculated with respect to the camera's position in 3D space.
+	This means that z and x inputs are "dampened" when the camera is looking at the marble from above.
+	
+	There's gotta be a way to implement the relative camera basis such that it informs relevant directional 
+	intention, while keeping the magnitude consistent regardless of where the camera is.
+"""
 func movement(delta) :
+	# Get inputs from user to calculate intended direction
 	var f_input = Input.get_action_raw_strength("ui_down") - Input.get_action_raw_strength("ui_up")
 	var h_input = Input.get_action_raw_strength("ui_right") - Input.get_action_raw_strength("ui_left")
 	
+	# Get position (a/k/a "transform") of camera
 	var camera_tranform = camera_3d.get_camera_transform()
 	var relative_camera_direction_z = camera_tranform.basis.z.normalized()
 	var relative_camera_direction_x = camera_tranform.basis.x.normalized()
 	
+	# Calculate forward/backward direction relative to camera position
 	var direction_f = f_input * relative_camera_direction_z
-	var direction_h = h_input * relative_camera_direction_x
 	
+	# Removes ability to "fly" by pointing camera parallel to y-axis and "pushing" or "pulling"
+	direction_f.y = 0
+	
+	# Calculate right/left direction relative to camera position
+	var direction_h = h_input * relative_camera_direction_x
+
+	# Apply force to marble in intended relative direction
 	apply_central_force(direction_f * movement_speed * delta)
 	apply_central_force(direction_h * movement_speed * delta)
 	pass
