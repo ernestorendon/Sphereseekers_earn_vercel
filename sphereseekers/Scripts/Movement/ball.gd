35daa06158f787dd616b89b92e61f2c69d43afaf
Ernesto <ernestorendon@ufl.edu>
2025-02-12 18:16:27 -0500

    Consolidate ball physics calculation functions

--- sphereseekers/Scripts/Movement/ball.gd
+++ sphereseekers/Scripts/Movement/ball.gd
@@ -1,18 +1,9 @@
 extends RigidBody3D
-@export var movement_speed : float = 98500.0
-@export var max_velocity : float = 20.0
+@export var movement_speed : float = 20.0
+@export var max_linear_velocity : float = 20.0
+@export var max_angular_velocity : float = 20.0
 
 @onready var camera_3d: Camera3D = $"../CameraRig/HRotation/VRotation/SpringArm3D/Camera3D"
-
-
-func _physics_process(delta):
-	# Set speed limit for the ball's velocity
-	linear_velocity.x = clamp(linear_velocity.x, -max_velocity, max_velocity)
-	linear_velocity.z = clamp(linear_velocity.z, -max_velocity, max_velocity)
-	
-	# Process physics of movement
-	movement(delta)
-	pass
 
 """
 TODO:
@@ -22,7 +13,12 @@ TODO:
 	There's gotta be a way to implement the relative camera basis such that it informs relevant directional 
 	intention, while keeping the magnitude consistent regardless of where the camera is.
 """
-func movement(delta) :
+func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	
+	# Set speed limits for speed and rotation speed of marble
+	linear_velocity.clampf(-max_linear_velocity, max_linear_velocity)
+	angular_velocity.clampf(-max_angular_velocity, max_angular_velocity)
+	
 	# Get inputs from user to calculate intended direction
 	var f_input = Input.get_action_raw_strength("ui_down") - Input.get_action_raw_strength("ui_up")
 	var h_input = Input.get_action_raw_strength("ui_right") - Input.get_action_raw_strength("ui_left")
@@ -40,11 +36,37 @@ func movement(delta) :
 	
 	# Calculate right/left direction relative to camera position
 	var direction_h = h_input * relative_camera_direction_x
+	
+	print("Forward Before force: ", direction_f)
+	print("Horizonatal Before force: ", direction_h)
+	apply_central_force(direction_f * movement_speed * get_physics_process_delta_time())
+	apply_central_force(direction_h * movement_speed * get_physics_process_delta_time())
+	
+		# Ball will not move around while shift is being held down...
+	if (Input.is_action_pressed("shift")):
+		if (Input.is_action_pressed("spacebar")):
+			#set_linear_velocity(Vector3.ZERO)
+			#set_angular_velocity(Vector3.ZERO)
+			#set_inertia(Vector3.ZERO)
+			print("SHIFT + SPACEBAR PRESSED")
+			print("Rotation impulse direction matrix: ", direction_f)
+			print("Angular velocity: ", get_angular_velocity())
+			apply_torque_impulse(direction_f + Vector3(0,0,5))
+			
+			#print(direction_h)
+			#apply_torque_impulse(direction_f)
+	
+		
+		## Ball will not move around while shift is being held down...
+	#if (Input.is_action_pressed("shift")):
+		#if (Input.is_action_pressed("spacebar")):
+			##set_linear_velocity(Vector3.ZERO)
+			##set_angular_velocity(Vector3.ZERO)
+			##set_inertia(Vector3.ZERO)
+			#print("Angular velocity: ", get_angular_velocity())
+			#apply_torque_impulse(Vector3(10,0,10))
+			#print("shift + spacebar pressed")
 
-	# Apply force to marble in intended relative direction
-	apply_central_force(direction_f * movement_speed * delta)
-	apply_central_force(direction_h * movement_speed * delta)
-	pass
 
 # Collision detection method
 func _on_body_entered(body: Node3D) -> void:
@@ -55,7 +77,9 @@ func _on_body_entered(body: Node3D) -> void:
 
 # Resets player position to (0, 5, -67.5)
 func reset_position() -> void:
-	linear_velocity = Vector3.ZERO # Stop ball
+	set_inertia(Vector3.ZERO)
+	set_linear_velocity(Vector3.ZERO) # Stop ball
+	set_angular_velocity(Vector3.ZERO)
 	var new_transform = global_transform
 	new_transform.origin = Vector3(0, 5, -67.5) # Set position to (0, 5, -67.5)
 	global_transform = new_transform
