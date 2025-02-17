2c0a186a36d1cdd23dfd6d51bd6308bde9b00c9f
Kenet K Ortiz <kenethortiz2016@gmail.com>
2025-02-17 09:18:06 -0500

    Phone deployment finished

--- sphereseekersWebsite/src/app/page.js
+++ sphereseekersWebsite/src/app/page.js
@@ -1,7 +1,21 @@
 'use client';
 import GameEmbed from './GameEmbed';
+import {isMobile} from 'react-device-detect';
 
 export default function Page() {
+
+  if (isMobile) {
+      return (
+        <div className='page'>
+          <h1 className='title'>
+            Sphereseekers Mobile
+          </h1>
+          <div className='container'>
+            <GameEmbed />
+          </div>
+        </div>
+      );
+  }
   return (
     <div className='page'>
       <h1 className='title'>
