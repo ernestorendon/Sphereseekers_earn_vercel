b86ca976baf933e7f953138333137ffd39c9f5ba
Kenet K Ortiz <kenethortiz2016@gmail.com>
2025-02-17 17:24:47 -0500

    Website modified for phone view, title removed and different styling for phone

--- sphereseekersWebsite/src/app/page.js
+++ sphereseekersWebsite/src/app/page.js
@@ -3,27 +3,11 @@ import GameEmbed from './GameEmbed';
 import {isMobile} from 'react-device-detect';
 
 export default function Page() {
-
-  if (isMobile) {
-      return (
-        <div className='page'>
-          <h1 className='title'>
-            Sphereseekers Mobile
-          </h1>
-          <div className='container'>
-            <GameEmbed />
-          </div>
-        </div>
-      );
-  }
   return (
     <div className='page'>
-      <h1 className='title'>
-        Sphereseekers
-      </h1>
       <div className='container'>
         <GameEmbed />
       </div>
     </div>
-  );
+  )
 }
