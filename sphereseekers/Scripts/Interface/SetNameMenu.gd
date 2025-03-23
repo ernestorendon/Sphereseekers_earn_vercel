0b3f0cf15a92869e8bfbf89d1bc55cd128ea2560
Ernesto <ernestorendon@ufl.edu>
2025-03-23 13:08:09 -0400

    Set name menu update

--- sphereseekers/Scripts/Interface/SetNameMenu.gd
+++ sphereseekers/Scripts/Interface/SetNameMenu.gd
@@ -1,165 +1,149 @@
-extends Node2D
+extends Control
 
-var label
-var error_label
+var title_label: TextureRect
+var error_label: Label
 var name_input: LineEdit
-var continue_button
-	
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	label = $title
-	name_input = $name_input
-	name_input.gui_input.connect(_on_name_input_gui_input)
-	# Enable virtual keyboard
-	name_input.virtual_keyboard_enabled = true
-	# Set focus mode
-	name_input.focus_mode = Control.FOCUS_ALL
-	continue_button = $continue
-	error_label = $error
-	
-	if not Global.is_mobile:
-		set_objects_for_desktop(label, name_input, continue_button)
-	else:
-		# we assume that is a smartphone
-		set_objects_for_smartphone(label, name_input, continue_button)
-		name_input.grab_focus()  # This forces the input box to gain focus
+var continue_btn: TextureButton
+var bg_rect: ColorRect
 
-func _input(event):
-	if event is InputEventScreenTouch and event.pressed:
-		var rect = name_input.get_global_rect()
-		if rect.has_point(event.position):
-			print("Global touch on input field")
+func _ready() -> void:
+	title_label = $title
+	name_input = $name_input
+	continue_btn = $continue
+	error_label = $error
+	bg_rect = $background
+
+	name_input.gui_input.connect(_on_name_input_gui_input)
+	
+	if Global.is_mobile:
+		set_mobile_layout()
+	else:
+		set_desktop_layout()
+
+func _on_name_input_gui_input(event: InputEvent) -> void:
+	if Global.is_mobile:
+		if event is InputEventScreenTouch and event.pressed:
 			name_input.grab_focus()
 
-func _on_name_input_gui_input(event):
-	if event is InputEventScreenTouch and event.pressed:
-		print("Input tapped!")  # Debugging line
-		name_input.release_focus()  # First release any existing focus
-		name_input.grab_focus()     # Then grab focus again
-		OS.request_permissions()    # Request system permissions if needed (might help on some devices)
+func _on_continue_pressed() -> void:
+	var name = name_input.text.strip_edges()
 
-
-func _on_continue_pressed():
-	var trimmed_text = name_input.text.strip_edges()
-	
-	if trimmed_text == "":
+	if name == "":
 		error_label.text = "Please enter your name"
-		error_label.modulate = Color(1, 0, 0)
+		error_label.modulate = Color(1, 0, 0, 0) # Start transparent
 		error_label.visible = true
+
+		var tween = create_tween()
+		tween.tween_property(error_label, "modulate", Color.RED, 0.4)
 		return
-		
+
 	error_label.visible = false
 	var save_names: Array = LocalStorage.get_save_names()
-	
-	if trimmed_text in save_names:
-		_on_override_confirm(trimmed_text)
+
+	if name in save_names:
+		show_override_dialog(name)
 	else:
-		save_names.append(trimmed_text)
+		save_names.append(name)
 		LocalStorage.set_save_names(save_names)
-		
-		# Change the scene to Level1
 		Global.is_paused = false
-		get_tree().change_scene_to_file("res://Scenes/Levels/Tutorial.tscn")
+		get_tree().change_scene_to_file("res://Scenes/Interface/loading_screen.tscn")
 
-func _on_override_confirm(save_name):
-	var popup = ConfirmationDialog.new()
-	popup.dialog_text = "Save with this name already exists. Are you sure you want to override that save?"
-	popup.ok_button_text = "Override"
-	popup.cancel_button_text = "Exit"
-	popup.get_ok_button().modulate = Color.RED
-	add_child(popup)
-	popup.confirmed.connect(_on_override_pressed.bind(save_name))
-	popup.popup_centered()
+func show_override_dialog(name: String) -> void:
+	var dialog := ConfirmationDialog.new()
+	dialog.dialog_text = "Save with this name already exists. Are you sure you want to override that save?"
+	dialog.ok_button_text = "Override"
+	dialog.cancel_button_text = "Exit"
+	dialog.get_ok_button().modulate = Color.RED
+	add_child(dialog)
+	dialog.confirmed.connect(_on_override_confirmed.bind(name))
+	dialog.popup_centered()
 
-func _on_override_pressed(save_name):
+func _on_override_confirmed(_name: String) -> void:
 	Global.is_paused = false
 	get_tree().change_scene_to_file("res://Scenes/Levels/Tutorial.tscn")
 
-func set_objects_for_desktop(label, name_input, continue_button):
-	var screen_size = get_viewport_rect().size
-	var width = screen_size.x
-	var height = screen_size.y
+func set_desktop_layout() -> void:
+	var size = get_viewport_rect().size
+	var w = size.x
+	var h = size.y
+
+	bg_rect.set_size(size)
+	bg_rect.set_position(Vector2.ZERO)
+	bg_rect.color = Color(173 / 255.0, 216 / 255.0, 230 / 255.0)
 
 	# Title
-	label.set_size(Vector2(width * 0.75, height * 0.15))
-	label.text = "Set Name"
-	label.set_position(Vector2(
-		(width - label.size.x) / 2,
-		(height - label.size.y) / 10
-	))
-	label.add_theme_font_size_override("font_size", 48)
-	label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-	label.visible = true
+	title_label.set_size(Vector2(400, 200))
+	var title_target_pos = Vector2((w - title_label.size.x) / 2, h * 0.08)
+	title_label.position = Vector2(title_target_pos.x, -title_label.size.y)  # start above
+	animate_property(title_label, "position", title_target_pos, 1.0, Tween.TRANS_BOUNCE)
 
-	# LineEdit
-	name_input.set_size(Vector2(width * 0.75, height * 0.08))
-	name_input.set_position(Vector2(
-		(width - name_input.size.x) / 2,
-		label.position.y + label.size.y + height * 0.2
-	))
+	# Input
+	name_input.set_size(Vector2(w * 0.75, h * 0.08))
+	var input_target_pos = Vector2((w - name_input.size.x) / 2, title_target_pos.y + title_label.size.y + h * 0.2)
+	name_input.position = Vector2(-name_input.size.x, input_target_pos.y)
+	animate_property(name_input, "position", input_target_pos, 0.6)
+
 	name_input.placeholder_text = "Enter your name"
 	name_input.visible = true
 
 	# Continue Button
-	continue_button.set_size(Vector2(width * 0.5, height * 0.08))
-	continue_button.set_position(Vector2(
-		(width - continue_button.size.x) / 2,
-		name_input.position.y + name_input.size.y + height * 0.1 
-	))
-	continue_button.text = "Continue"
-	continue_button.visible = true
+	continue_btn.set_size(Vector2(200, 100))
+	var button_target_pos = Vector2((w - continue_btn.size.x) / 2, input_target_pos.y + name_input.size.y + h * 0.1)
+	continue_btn.position = Vector2(w + continue_btn.size.x, button_target_pos.y)
+	animate_property(continue_btn, "position", button_target_pos, 0.6, Tween.TRANS_SINE, Tween.EASE_OUT)
 
 	# Error Label
-	error_label.set_size(Vector2(width * 0.75, height * 0.05))
-	error_label.set_position(Vector2(
-		(width - error_label.size.x) / 2,
-		continue_button.position.y + continue_button.size.y + height * 0.1
-	))
+	error_label.set_size(Vector2(100, 50))
+	error_label.set_position(Vector2((w - error_label.size.x) / 2, button_target_pos.y + continue_btn.size.y + h * 0.1))
 	error_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-	error_label.modulate = Color(1, 0, 0)
+	error_label.modulate = Color.RED
 	error_label.visible = false
 
-func set_objects_for_smartphone(label, name_input, continue_button):
-	var screen_size = get_viewport_rect().size
-	var width = screen_size.x
-	var height = screen_size.y
+func set_mobile_layout() -> void:
+	var size = get_viewport_rect().size
+	var w = size.x
+	var h = size.y
+
+	bg_rect.set_size(size)
+	bg_rect.set_position(Vector2.ZERO)
+	bg_rect.color = Color(173 / 255.0, 216 / 255.0, 230 / 255.0)
 
 	# Title
-	label.set_size(Vector2(width * 0.75, height * 0.15))
-	label.text = "Set Name"
-	label.set_position(Vector2(
-		(width - label.size.x) / 2,
-		(height - label.size.y) / 10
-	))
-	label.add_theme_font_size_override("font_size", 48)
-	label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-	label.visible = true
+	title_label.text = "Set Name"
+	title_label.set_size(Vector2(w * 0.75, h * 0.15))
+	var title_target_pos = Vector2((w - title_label.size.x) / 2, h * 0.08)
+	title_label.position = Vector2(title_target_pos.x, -title_label.size.y)
+	animate_property(title_label, "position", title_target_pos, 1.0, Tween.TRANS_BOUNCE)
 
-	# LineEdit
-	name_input.set_size(Vector2(width * 0.75, height * 0.08))
-	name_input.set_position(Vector2(
-		(width - name_input.size.x) / 2,
-		label.position.y + label.size.y + height * 0.2
-	))
+	title_label.add_theme_font_size_override("font_size", 48)
+	title_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	title_label.visible = true
+
+	# Input
+	name_input.set_size(Vector2(w * 0.8, h * 0.08))
+	var input_target_pos = Vector2((w - name_input.size.x) / 2, title_target_pos.y + title_label.size.y + h * 0.05)
+	name_input.position = Vector2(-name_input.size.x, input_target_pos.y)
+	animate_property(name_input, "position", input_target_pos, 0.6)
+
 	name_input.placeholder_text = "Enter your name"
 	name_input.visible = true
 
 	# Continue Button
-	continue_button.set_size(Vector2(width * 0.5, height * 0.08))
-	continue_button.set_position(Vector2(
-		(width - continue_button.size.x) / 2,
-		name_input.position.y + name_input.size.y + height * 0.1 
-	))
-	continue_button.text = "Continue"
-	continue_button.visible = true
+	continue_btn.set_size(Vector2(w * 0.5, h * 0.08))
+	var button_target_pos = Vector2((w - continue_btn.size.x) / 2, input_target_pos.y + name_input.size.y + h * 0.05)
+	continue_btn.position = Vector2(w + continue_btn.size.x, button_target_pos.y)
+	animate_property(continue_btn, "position", button_target_pos, 0.6)
+
+	continue_btn.text = "Continue"
+	continue_btn.visible = true
 
 	# Error Label
-	error_label.set_size(Vector2(width * 0.75, height * 0.05))
-	error_label.set_position(Vector2(
-		(width - error_label.size.x) / 2,
-		continue_button.position.y + continue_button.size.y + height * 0.1
-	))
+	error_label.set_size(Vector2(w * 0.8, h * 0.05))
+	error_label.set_position(Vector2((w - error_label.size.x) / 2, button_target_pos.y + continue_btn.size.y + h * 0.05))
 	error_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-	error_label.modulate = Color(1, 0, 0)
+	error_label.modulate = Color.RED
 	error_label.visible = false
-	
+
+func animate_property(node: Node, property: String, target_value: Variant, duration: float, transition := Tween.TRANS_SINE, ease := Tween.EASE_OUT) -> void:
+	var tween = create_tween()
+	tween.tween_property(node, property, target_value, duration).set_trans(transition).set_ease(ease)
